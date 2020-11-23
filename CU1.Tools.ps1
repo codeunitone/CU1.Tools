7770f8a203db5a1b06fa1a22aa20de7a8c568ce4
Benjamin Alisch <benjamin.alisch@misterspex.de>
2020-11-23 16:42:49 +0100

    CU1.Tools.Git: Add module to starter script

--- CU1.Tools.ps1
+++ CU1.Tools.ps1
@@ -1,2 +1,3 @@
 Import-Module $(Join-Path $PSScriptRoot -ChildPath 'CU1.Tools.Helper\CU1.Tools.Helper.psd1') -Force -DisableNameChecking
 Import-Module $(Join-Path $PSScriptRoot -ChildPath 'CU1.Tools.PowerShell\CU1.Tools.PowerShell.psd1') -Force -DisableNameChecking
+Import-Module $(Join-Path $PSScriptRoot -ChildPath 'CU1.Tools.Git\CU1.Tools.Git.psd1') -Force -DisableNameChecking
