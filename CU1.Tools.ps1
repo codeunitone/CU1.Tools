100986769f570e9862e3c295dd239d73a36af737
Benjamin Alisch <benjamin.alisch@misterspex.de>
2022-05-23 16:54:03 +0200

    CU1.Tools.Sql: load module

--- CU1.Tools.ps1
+++ CU1.Tools.ps1
@@ -1,3 +1,4 @@
 Import-Module $(Join-Path $PSScriptRoot -ChildPath 'CU1.Tools.Helper\CU1.Tools.Helper.psd1') -Force -DisableNameChecking
 Import-Module $(Join-Path $PSScriptRoot -ChildPath 'CU1.Tools.PowerShell\CU1.Tools.PowerShell.psd1') -Force -DisableNameChecking
 Import-Module $(Join-Path $PSScriptRoot -ChildPath 'CU1.Tools.Git\CU1.Tools.Git.psd1') -Force -DisableNameChecking
+Import-Module $(Join-Path $PSScriptRoot -ChildPath 'CU1.Tools.Sql\CU1.Tools.Sql.psd1') -Force -DisableNameChecking
